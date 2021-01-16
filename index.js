7d07a98c5e6ee8033c2362f822f04edc7c02292b
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-16 15:41:00 +0530

    Add index.js

---
+++ index.js
@@ -0,0 +1,81 @@
+const { randomBytes } = require("crypto")
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const { InstallProvider } = require('@slack/oauth')
+const { App, ExpressReceiver } = require('@slack/bolt')
+
+const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET })
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    receiver
+})
+
+const jia = "U01HJ78R466"
+const invalidReaction = "bangbang"
+
+const installer = new InstallProvider({
+    clientId: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET,
+    stateSecret: randomBytes(20).toString('hex'),
+    installationStore: {
+        storeInstallation: async (installation) => {
+            await prisma.user.upsert({
+                where: { slackID: installation.user.id },
+                create: {
+                    slackID: installation.user.id,
+                    installation: JSON.stringify(installation)
+                },
+                update: {
+                    installation: JSON.stringify(installation)
+                }
+            })
+        },
+        fetchInstallation: async (InstallQuery) => {
+            const user = await prisma.user.findUnique({
+                where: {
+                    slackID: InstallQuery.userId
+                }
+            })
+            return JSON.parse(user.installation)
+        },
+    },
+})
+
+app.event('reaction_added', async ({ event }) => {
+    if (event.user === jia && event.reaction === invalidReaction) {
+        try {
+            const result = await installer.authorize({ userId: event.item_user })
+            await app.client.chat.delete({
+                token: result.userToken,
+                channel: event.item.channel,
+                ts: event.item.ts
+            })
+        } catch {
+            // Not authed
+        }
+    }
+})
+
+async function main() {
+    await app.start(process.env.PORT || 3000)
+    console.log('Lightning Delete running ⚡️')
+}
+main()
+
+receiver.router.get('/', async (_, res) => {
+    const url = await installer.generateInstallUrl({
+        scopes: ['channels:history', 'reactions:read'],
+        userScopes: ["chat:write"],
+    })
+    res.redirect(url)
+})
+
+receiver.router.get('/slack/oauth_redirect', async (req, res) => {
+    await installer.handleCallback(req, res)
+})
+
+receiver.router.get('/ping', async (_, res) => {
+    res.send("Online")
+})
